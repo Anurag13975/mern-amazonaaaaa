7e6f5d3f8a9e338605c722c2b010faa9969e9c5e
Anurag <anuragsinghal6072@gmail.com>
2023-07-16 15:57:34 +0530

    19-Connect-To-MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,18 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 const app = express();
 
 app.get('/api/products', (req, res) => {
@@ -11,8 +23,7 @@ app.get('/api/products/slug/:slug', (req, res) => {
   const product = data.products.find((x) => x.slug === req.params.slug);
   if (product) {
     res.send(product);
-  }
-  else {
+  } else {
     res.status(404).send({ message: 'Product Not Found' });
   }
 });
@@ -21,12 +32,10 @@ app.get('/api/products/:id', (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
     res.send(product);
-  }
-  else {
+  } else {
     res.status(404).send({ message: 'Product Not Found' });
   }
 });
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
