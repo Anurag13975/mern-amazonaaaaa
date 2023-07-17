e16a276ea79ee3d3fea789c832259ec85f2106ef
Anurag <anuragsinghal6072@gmail.com>
2023-07-17 23:57:08 +0530

    29-Order-Screen

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -19,4 +19,17 @@ orderRouter.post('/',isAuth,expressAsyncHandler(async (req, res) => {
     const order = await newOrder.save();
     res.status(201).send({ message: 'New Order Created', order });
 }));
+
+
+orderRouter.get('/:id',isAuth,expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(404).send({ message: 'Order Not Found' });
+    }
+  })
+);
+
+
 export default orderRouter;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SignupScreen from './screens/SignupScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 
 function App() {
@@ -85,6 +86,7 @@ function App() {
               <Route path="/shipping" element={<ShippingAddressScreen />} />
               <Route path="/payment" element={<PaymentMethodScreen />}></Route>
               <Route path="/placeorder" element={<PlaceOrderScreen />} />
+              <Route path="/order/:id" element={<OrderScreen />}></Route>
             </Routes>
           </Container>
         </main>
